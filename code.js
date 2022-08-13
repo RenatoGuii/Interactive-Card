3d2bee0cc8d2f4e19877248ced9928153fa9ee70
Renato Guimarães <renatoguidev@gmail.com>
2022-08-13 15:40:24 -0300

    13/08 pt2
    JavaScript start

--- code.js
+++ code.js
@@ -1,1 +1,10 @@
 /*AJEITAR PARA QUE SÓ RECEBA LETRAS NO PRIMEIRO INPUT*/ 
+let cardHolderName = document.querySelector("#cardHolderTxt")
+let cardNumber = document.querySelector("cardNumberTxt")
+let month = document.querySelector("#mm")
+let year = document.querySelector("yy")
+let cvc = document.querySelector("#cvcNumber")
+
+function confirm () {
+
+}
