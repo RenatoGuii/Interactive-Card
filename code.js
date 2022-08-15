b9203907e231e0146683236b1ae91c3cc105d336
Renato Guimarães <renatoguidev@gmail.com>
2022-08-15 19:12:51 -0300

    15/08
    95%

--- code.js
+++ code.js
@@ -1,21 +1,30 @@
-/*AJEITAR PARA QUE SÓ RECEBA LETRAS NO PRIMEIRO INPUT*/ 
 let cardHolderName = document.querySelector("#cardHolderTxt")
 let cardNumber = document.querySelector("#cardNumberTxt")
 let month = document.querySelector("#mm")
 let year = document.querySelector("#yy")
 let cvc = document.querySelector("#cvcNumber")
+let yourNameTXT = document.querySelector ("#yourName")
+let numberCardTXT = document.querySelector ("#cardNumberToJS")
+let mmCardTXT = document.querySelector ("#expDateMM")
+let yyCardTXT = document.querySelector ("#expDateYY")
+let cvcCardTXT = document.querySelector ("#cvcCard")
 
 
 function buttonConfirm () {
-    let alertDate = document.querySelector ("#alertDateTxt")
-    if (cardHolderName.value.length != 0 && cardNumber.length != 0 && Number(cardNumber.value) > 0 && month.value.length != 0 && year.value.length != 0 && cvc.value.length != 0) {
+    data = new Date().getFullYear().toString().substr(-2);
+    
+    if (cardHolderName.value.length != 0 && cardNumber.value.length == 16 && Number(cardNumber.value) > 0 && month.value.length == 2 && Number(month.value) >=1 && Number(month.value) <= 12 && year.value.length == 2 && year.value >= data && cvc.value.length == 3) {
         let form = document.querySelector ("#containerWhite")
         let complete = document.querySelector("#completeState")
         form.style.display = "none";
         complete.style.display = "block";
+        yourNameTXT.innerHTML = `${(cardHolderName.value).toUpperCase()}`
+        numberCardTXT.innerHTML = `${(cardNumber.value)}`
+        mmCardTXT.innerHTML = `${(month.value).toUpperCase()}`
+        yyCardTXT.innerHTML = `${(year.value).toUpperCase()}`
+        cvcCardTXT.innerHTML = `${(cvc.value).toUpperCase()}`
     } else {
-         
-        window.alert ("[ERRO] Check your data!")
+        window.alert (" [ERRO] Check your data! \n - No blank spaces allowed \n - Card Number must be 16 digits \n - Month and Year must be 2 digits \n - CVC must be 3 digits ")
     }
 }
 
@@ -29,4 +38,76 @@ function buttonContinue () {
     month.value = null
     year.value = null
     cvc.value = null
+    yourNameTXT.innerHTML = "YOUR NAME"
+    numberCardTXT.innerHTML = "0000 0000 0000 0000"
+    mmCardTXT.innerHTML = "00"
+    yyCardTXT.innerHTML = "00"
+    cvcCardTXT.innerHTML = "000"
+}
+
+
+/*Digits limitation*/ 
+
+cardHolderName.addEventListener("keypress", function(e) {
+    var keyCode = (e.keyCode ? e.keyCode : e.which);
+  
+  if (keyCode > 47 && keyCode < 58) {
+    e.preventDefault();
+  }
+});
+
+cardNumber.addEventListener("keypress", function(e) {
+    if(!checkChar(e)) {
+      e.preventDefault();
+  }
+});
+function checkChar(e) {
+    var char = String.fromCharCode(e.keyCode);
+  
+    var pattern = '[0-9]';
+    if (char.match(pattern)) {
+      return true;
+  }
+}
+
+month.addEventListener("keypress", function(e) {
+    if(!checkChar(e)) {
+      e.preventDefault();
+  }
+});
+function checkChar(e) {
+    var char = String.fromCharCode(e.keyCode);
+  
+    var pattern = '[0-9]';
+    if (char.match(pattern)) {
+      return true;
+  }
+}
+
+year.addEventListener("keypress", function(e) {
+    if(!checkChar(e)) {
+      e.preventDefault();
+  }
+});
+function checkChar(e) {
+    var char = String.fromCharCode(e.keyCode);
+  
+    var pattern = '[0-9]';
+    if (char.match(pattern)) {
+      return true;
+  }
+}
+
+cvc.addEventListener("keypress", function(e) {
+    if(!checkChar(e)) {
+      e.preventDefault();
+  }
+});
+function checkChar(e) {
+    var char = String.fromCharCode(e.keyCode);
+  
+    var pattern = '[0-9]';
+    if (char.match(pattern)) {
+      return true;
+  }
 }
